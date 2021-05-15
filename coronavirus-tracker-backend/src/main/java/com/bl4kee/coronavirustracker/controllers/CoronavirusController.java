62f9cc1c4c9b03510d7fbe2b04c51ab800624633
Bartek <bl4kee@wp.pl>
2021-05-15 14:15:07 +0200

    Enabled crossOrigin for fetching data.

--- coronavirus-tracker-backend/src/main/java/com/bl4kee/coronavirustracker/controllers/CoronavirusController.java
+++ coronavirus-tracker-backend/src/main/java/com/bl4kee/coronavirustracker/controllers/CoronavirusController.java
@@ -3,6 +3,7 @@ package com.bl4kee.coronavirustracker.controllers;
 import com.bl4kee.coronavirustracker.entities.LocationStats;
 import com.bl4kee.coronavirustracker.services.CoronavirusDataService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class CoronavirusController {
 
     private final CoronavirusDataService coronavirusDataService;
 
+    @CrossOrigin
     @GetMapping("/")
     public List<LocationStats> fetchCoronavirusData() {
         return coronavirusDataService.getAllStats();
